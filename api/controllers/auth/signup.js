89481a770cacbafb24ec674cb38ccc1a0ec3ee49
Arnab <arnangoswamiam@gmail.com>
2023-11-13 23:06:47 +0530

    create signup controller for saving new user to DB

--- api/controllers/auth/signup.js
+++ api/controllers/auth/signup.js
@@ -1,6 +1,22 @@
-const signup = (req, res, next) => {
-  const data = req.body;
-  console.log(data);
+import User from '../../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ 
+    username, 
+    email, 
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.status(201).json({
+      message: 'User created successfully!',
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 export default signup;
