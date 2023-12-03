8de0cb923699cdbf37d3e3d518632482a8300754
Arnab <arnangoswamiam@gmail.com>
2023-12-04 00:41:26 +0530

    pfp image upload to firebase storage functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,12 +1,79 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
 import { useSelector } from 'react-redux';
 
+import getFileNameWithTime from '../utils/getFileNameWithTime.js';
+
+import { 
+  getDownloadURL, 
+  getStorage, 
+  ref, 
+  uploadBytesResumable 
+} from 'firebase/storage';
+import { app } from '../firebase.js';
+
+const FileUploadMessage = ({ percent, error, setPercent }) => {
+  const [successTimeout, setSuccessTimeout] = useState(null);
+
+  const setNewSuccessTimeout = () => {
+    if (successTimeout) {
+      clearTimeout(successTimeout);
+    }
+    const timeout = setTimeout(() => {
+      setPercent(0);
+    }, 3000);
+    setSuccessTimeout(timeout);
+  };
+
+  useEffect(() => {
+    if (percent === 100) {
+      setNewSuccessTimeout();
+    }
+  }, [percent]);
+
+  let message = null;
+  if (typeof error === 'object' && error) {
+    message = 
+    <span className='text-red-700'>
+      File must be less than 2mb and an image!
+    </span>;
+  } else if (typeof error === 'string' && error) {
+    message = 
+    <span className='text-red-700'>
+      {error}
+    </span>;
+  } else if (percent > 0 && percent < 100) {
+    message = 
+    <span className='text-slate-700'>
+      {`Uploading ${percent}%`}
+    </span>;
+  } else if (percent === 100) {
+    message = 
+    <span className='text-green-700'>
+      Image successfully uploaded!
+    </span>;
+  }
+
+  return (
+    <p className='text-center'>
+      {message}
+    </p>
+  );
+};
+
 const Profile = () => {
   const { currentUser } = useSelector(state => state.user);
 
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    username: currentUser.username,
+    email: currentUser.email,
+    oldPassword: '',
+    password: '',
+    confirmPassword: '',
+    avatar: currentUser.avatar,
+  });
+
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
@@ -40,7 +107,7 @@ const Profile = () => {
     try {
       setLoading(true);
 
-      const res = await fetch('/api/auth/signup', {
+      const res = await fetch('/api/auth/update', {
         method:'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -50,6 +117,7 @@ const Profile = () => {
           email: formData.email,
           oldPassword: formData.oldPassword,
           password: formData.password,
+          avatar: formData.avatar,
         }),
       });
 
@@ -103,6 +171,101 @@ const Profile = () => {
   const [oldPasswordVisible, setOldPasswordVisible] = useState(false);
   const [focusOldPassword, setFocusOldPassword] = useState(false);
 
+  // Firebase storage rules:
+  /*
+  rules_version = '2';
+
+  // Craft rules based on data in your Firestore database
+  // allow write: if firestore.get(
+  //    /databases/(default)/documents/users/$(request.auth.uid)).data.isAdmin;
+  service firebase.storage {
+    match /b/{bucket}/o {
+      match /{allPaths=**} {
+        allow read;
+        allow write: if 
+        request.resource.size < 2 * 1024 * 1024 &&
+        request.resource.contentType.matches('image/.*')
+      }
+    }
+  }
+  */
+
+  const fileRef = useRef(null);
+
+  const [file, setFile ] = useState(undefined);
+
+  const [fileUploadPercentage, setFileUploadPercentage] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(null);
+
+  const [fileUploadErrorTimeout, setFileUploadErrorTimeout] = useState(null);
+  const setNewFileUploadErrorTimeout = () => {
+    if (fileUploadErrorTimeout) {
+      clearTimeout(fileUploadErrorTimeout);
+    }
+    setFileUploadErrorTimeout(
+      setTimeout(() => {
+        setFileUploadError(null);
+      }, 3000)
+    );
+  };
+  useEffect(() => {
+    return () => {
+      if (fileUploadErrorTimeout) {
+        clearTimeout(fileUploadErrorTimeout);
+      }
+    };
+  }, [fileUploadErrorTimeout]);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const filename = getFileNameWithTime(file.name);
+    const storageRef = ref(storage, filename);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFileUploadPercentage(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(error);
+        setNewFileUploadErrorTimeout();
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then( (downloadURL) => {
+          setFormData({ 
+            ...formData, 
+            avatar: downloadURL 
+          });
+        });
+      },
+    );
+  };
+
+  console.log('File', file);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    // These checks are also present in the firebase storage rules, so they are not really required. But doing them here too is more efficient as it doesn't waste time fully uploading the file to the storage and then checking if it satisfies the storage's rules.
+    if (file.size > 2 * 1024 * 1024) {
+      setFileUploadError('File must be less than 2mb!');
+      setNewFileUploadErrorTimeout();
+      return;
+    }
+    if (!file.type.startsWith('image/')) {
+      setFileUploadError('File must be an image!');
+      setNewFileUploadErrorTimeout();
+      return;
+    }
+    setFile(file);
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>
@@ -111,11 +274,26 @@ const Profile = () => {
 
       <form className='flex flex-col gap-4' 
       onSubmit={handleSubmit}>
-        <img src={currentUser.avatar}
+        <input accept='image/*'
+        type='file' 
+        ref={fileRef} 
+        hidden 
+        onChange={handleFileChange}
+        />
+        <img onClick={ () => {
+          fileRef.current.click();
+        } }
+        src={formData.avatar}
+        id='avatar'
         alt='profile'
         className='rounded-full h-24 w-24 object-cover cursor-pointer mt-2 self-center' />
+        <FileUploadMessage 
+        error={fileUploadError}
+        percent={fileUploadPercentage}
+        setPercent={setFileUploadPercentage}
+        />
         <input type='text'
-        value={currentUser.username} 
+        value={formData.username} 
         placeholder='username' 
         className='border p-3 rounded-lg
         focus:bg-gray-100 focus:border-slate-700 focus:outline-none' 
@@ -128,7 +306,7 @@ const Profile = () => {
           setFocusConfirmPassword(false);
         } }/>
         <input type='email'
-        value={currentUser.email}
+        value={formData.email}
         placeholder='email' 
         className='border p-3 rounded-lg
         focus:bg-gray-100 focus:border-slate-700 focus:outline-none' 
---
+++ client/src/utils/getFileNameWithTime.js
@@ -0,0 +1,32 @@
+const getFileAndExtension = (filename) => {
+  const lastDotIndex = filename.lastIndexOf('.');
+  let file = '';
+  let extension = '';
+  if (lastDotIndex !== -1) {
+    file = filename.substring(0, lastDotIndex);
+    extension = filename.substring(lastDotIndex + 1);
+  } else {
+    file = filename;
+  }
+  return { file, extension };
+};
+
+const getFileNameWithTime = (filename) => {
+  const { file, extension } = getFileAndExtension(filename);
+
+  const timestamp = new Date().getTime();
+  const date = new Date(timestamp);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+  const newFileName = file + ' ' + formattedDate + '.' + extension;
+
+  return newFileName;
+};
+
+export default getFileNameWithTime;
