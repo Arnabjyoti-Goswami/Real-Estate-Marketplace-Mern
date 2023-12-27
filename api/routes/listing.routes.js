c61b9a0e2c1b7fb1b44b33b439d977f8ba02ce63
Arnab <arnangoswamiam@gmail.com>
2023-12-27 19:27:36 +0530

    create search listings api route

--- api/routes/listing.routes.js
+++ api/routes/listing.routes.js
@@ -5,6 +5,7 @@ import createListing from '../controllers/listing/createListing.js';
 import deleteListing from '../controllers/listing/deleteListing.js';
 import updateListing from '../controllers/listing/updateListing.js';
 import getListing from '../controllers/listing/getListing.js';
+import getListings from '../controllers/listing/getListings.js';
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get', getListings);
 
 export default router;
