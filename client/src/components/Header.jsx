6adef325ff3395323c93ff6843239d3b325d0c6f
Arnab <arnangoswamiam@gmail.com>
2023-12-27 20:28:44 +0530

    add header component + url searchTerm functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,55 @@
 import { FaSearch as SearchIcon } from 'react-icons/fa';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 
 const Header = () => {
   const { currentUser } = useSelector(state => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const navigate = useNavigate();
 
   const navOptions = [
     { label: 'Home', route: '/'},
     { label: 'About', route: '/about'},
     { label: 'Sign in', route: '/sign-in'},
   ];
+
+  const handleKeyDown = (e) => {
+    if(e.key === 'Enter') {
+      handleSubmit(e);
+    }
+  };
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  const updateUrl = (searchTerm) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+
+    navigate(`/search?${searchQuery}`, { replace: true });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateUrl(searchTerm);
+  }
+
+  const getSearchTermFromUrl = () => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+
+    if(searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  };
+
+  useEffect(() => {
+    getSearchTermFromUrl();
+  }, [location.search]);
 
   return (
     <header className='bg-slate-200 shadow-md'>
@@ -25,11 +65,18 @@ const Header = () => {
           </h1>
         </Link>
 
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form onSubmit={handleSubmit}
+        className='bg-slate-100 p-3 rounded-lg flex items-center'>
           <input 
           type="text" 
-          placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-          <SearchIcon className='text-slate-600' />
+          placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'
+          value={searchTerm}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          />
+          <button type='submit'>
+            <SearchIcon className='text-slate-600' />
+          </button>
         </form>
 
         <nav className='flex gap-4'>
@@ -48,13 +95,15 @@ const Header = () => {
           return ( 
             <NavLink to={item.route}
             key={`nav option #${index}`}
-            className={ ({isActive}) => { return `
-            text-slate-500 hover:underline
-            ${index !== navOptions.length - 1 ? 'hidden sm:inline' : ''}
-            ${isActive ? 
-              'text-bg-blue-gradient' 
-              : 
-              ''}
+            className={ ({isActive}) => { 
+            return `
+              text-slate-500 text-[17px]
+              hover:underline
+              ${index !== navOptions.length - 1 ? 'hidden sm:inline' : ''}
+              ${isActive ? 
+                'text-bg-blue-gradient font-semibold' 
+                : 
+                ''}
             `} }
             >
               {item.label}
