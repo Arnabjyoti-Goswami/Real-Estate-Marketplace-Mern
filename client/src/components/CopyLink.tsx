ee75091baa824923fdb8f4dad902da6c99b02e41
Arnab <arnangoswamiam@gmail.com>
2024-01-24 19:14:47 +0530

    for mutable .current of a ref, use | null in the type

--- client/src/components/CopyLink.tsx
+++ client/src/components/CopyLink.tsx
@@ -4,7 +4,7 @@ import { FaShare } from 'react-icons/fa';
 
 const CopyLink = () => {
   const [copied, setCopied] = useState(false);
-  const timeoutRef = useRef(null);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const handleCopyClick = () => {
     navigator.clipboard.writeText(window.location.href);
@@ -21,21 +21,24 @@ const CopyLink = () => {
 
   return (
     <>
-    <div className='fixed top-[13%] right-[3%] z-10 
-    border rounded-full w-12 h-12 
-    flex justify-center items-center 
-    bg-slate-100 cursor-pointer'
-    onClick={() => handleCopyClick()}>
-      <FaShare className='text-slate-500'/>
-    </div>
-    {copied && (
-      <p className='fixed top-[23%] right-[3%] z-10 
-      rounded-md bg-green-200 p-2 border border-green-700'
-      style={{ boxShadow: 'inset 0 0 10px rgba(0, 0, 0, 0.2)' }}
+      <div
+        className='fixed top-[13%] right-[3%] z-10 
+        border rounded-full w-12 h-12 
+        flex justify-center items-center 
+        bg-slate-100 cursor-pointer'
+        onClick={() => handleCopyClick()}
       >
-        Link copied!
-      </p>
-    )}
+        <FaShare className='text-slate-500' />
+      </div>
+      {copied && (
+        <p
+          className='fixed top-[23%] right-[3%] z-10 
+          rounded-md bg-green-200 p-2 border border-green-700'
+          style={{ boxShadow: 'inset 0 0 10px rgba(0, 0, 0, 0.2)' }}
+        >
+          Link copied!
+        </p>
+      )}
     </>
   );
 };
